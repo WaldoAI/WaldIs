3e0a43376c7a5e7cee455da77923f85174d1001f
BrandonPetersen <70966818+BrandonPetersen@users.noreply.github.com>
2022-05-20 13:32:14 -0700

    fixed import error

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -30,7 +29,7 @@ def after_request(response):
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+
 
     
 
